3445a1fff174bc4f00cfba3df5906c5a17c6adda
matousposp08 <154290575+matousposp08@users.noreply.github.com>
2024-05-21 20:54:54 -0400

    added some models, backgrounds

--- Dogfights/scripts/smooth_follow.gd
+++ Dogfights/scripts/smooth_follow.gd
@@ -1,20 +1,18 @@
-#############
-# Simple SmoothFollow Script
-#############
+
 class_name SmoothFollow
 extends Node3D
 
-# Target we are following
+
 @export var target:Node3D
-# The distance in the x-z plane to the target
+
 @export var distance:float = 10.0
-# the height we want the camera to be above the target
+
 @export var height:float = 5.0
-# Smooth the follow by damping it
+
 @export var position_damping:float = 2.0
-# Should we also look at the target?
+
 @export var should_rotate:bool = true
-#Smooth the rotation by damping it
+
 @export var rotation_damping:float = 3.0
 
 
@@ -30,14 +28,13 @@ func _process(delta):
 
 
 func _follow(delta):
-	# Set the position of the camera on the x-z plane to:
-	#	distance meters behind the target
+
 	var offset = -target.global_transform.basis.z * distance
 	var desired_position = target.position + offset
-	#   Set the height of the camera
+
 	desired_position.y += height
-	#   Lerp the final value
+
 	set_position(lerp(get_position(), desired_position, position_damping * delta))
-	#   Always look at the target
+
 	if (should_rotate == true):
 		self.quaternion = MathLib.smooth_look_at(self, target, rotation_damping, delta)
