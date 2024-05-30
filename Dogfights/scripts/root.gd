e7ce5575d257ea380c00e02da1bd86c15eb1bb8a
finsld <142837936+dVillarosa287@users.noreply.github.com>
2024-05-30 12:58:43 -0400

    asteroid: fix spawning error

--- Dogfights/scripts/root.gd
+++ Dogfights/scripts/root.gd
@@ -2,15 +2,18 @@ extends Node3D
 
 var ROCK : PackedScene = preload('res://scenes/asteroids.tscn')
 var rng = RandomNumberGenerator.new()
+var x = 90
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(0, 90):
-		rockSpawn(Vector3(rng.randf_range(-100.0, 100.0), rng.randf_range(-100.0, 100.0), rng.randf_range(-100.0, 100.0)))
-
+	#for i in range(0, 90):
+	#	rockSpawn(Vector3(rng.randf_range(-100.0, 100.0), rng.randf_range(-100.0, 100.0), rng.randf_range(-100.0, 100.0)))
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if (x > 0) :
+		x -= 1
+		rockSpawn(Vector3(rng.randf_range(-100.0, 100.0), rng.randf_range(-100.0, 100.0), rng.randf_range(-100.0, 100.0)))
 
 func rockSpawn(pos: Vector3):
 	var instance = ROCK.instantiate()
