938b6ae26fd8a21e314bbbdc52644e6b7843b0b4
matousposp08 <154290575+matousposp08@users.noreply.github.com>
2024-05-31 09:22:53 -0400

    level one almost finished, just need to finish NPC shooting

--- Dogfights/npc.gd
+++ Dogfights/npc.gd
@@ -6,12 +6,12 @@ extends CharacterBody3D
 @export var player: NodePath
 var x = 0
 
-var LASER : PackedScene = preload('res://scenes/laser.tscn')
+var LASER : PackedScene = preload('res://scenes/ship_laser.tscn')
 #var laser_instance: MeshInstance3D
 var shooting_timer: Timer
 
 func _physics_process(delta):
-	x += 1
+	x += 0.5
 	if x % 180 == 0:
 		laser(position, transform.basis)
 
