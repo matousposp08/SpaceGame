4748fb61e69ff0dda92f9997203257e915699cb0
finsld <142837936+dVillarosa287@users.noreply.github.com>
2024-05-31 09:02:57 -0400

    player: add damage functionality on collision

--- Dogfights/scripts/chargeShot.gd
+++ Dogfights/scripts/chargeShot.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 var SPEED = -300
 var x
+var explode = false
+var y = 20
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	scale *= 5
@@ -11,5 +13,23 @@ func _ready():
 func _process(delta):
 	position += transform.basis * Vector3(0,0,-SPEED) * delta
 	x -= 1
-	if (x < 1) :
+	if explode:
+		y -= 1
+	if (x < 1 or y < 1) :
 		queue_free()
+
+
+func destroy():
+	#print("yes")
+	explode = true
+	$GPUParticles3D.emitting = true
+	$Area3D/CollisionShape3D.disabled = true
+
+func _on_area_3d_area_entered(area):
+	if not area.is_in_group(get_groups()[0]):
+		destroy()
+
+
+func _on_area_3d_body_entered(body):
+	if not body.is_in_group(get_groups()[0]):
+		destroy()
