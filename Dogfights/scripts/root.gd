9ec5837792eb757c3b0aef3b99b48aef0e15f36d
Nikhil Palempalle <nikpal26@bergen.org>
2024-06-02 10:13:00 -0400

    Added star

--- Dogfights/scripts/root.gd
+++ Dogfights/scripts/root.gd
@@ -1,7 +1,9 @@
 extends Node3D
 
 var ROCK : PackedScene = preload('res://scenes/asteroids.tscn')
+var SUN : PackedScene = preload('res://scenes/star.tscn')
 var rng = RandomNumberGenerator.new()
+var SUN_added = false
 var x = 90
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +13,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if (!SUN_added):
+		var instance = SUN.instantiate()
+		instance.position = Vector3(100.0, 100.0, 0.0)
+		instance.scale *= 1
+		get_parent().add_child(instance)
+		SUN_added = true
 	if (x > 0) :
 		x -= 1
 		rockSpawn(Vector3(rng.randf_range(-100.0, 100.0), rng.randf_range(-100.0, 100.0), rng.randf_range(-100.0, 100.0)))
