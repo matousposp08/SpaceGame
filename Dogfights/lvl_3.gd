f692d8062815c6661d541ba69a12b23f63cccf67
matousposp08 <154290575+matousposp08@users.noreply.github.com>
2024-06-12 10:30:48 -0400

    level 3

---
+++ Dogfights/lvl_3.gd
@@ -0,0 +1,36 @@
+extends Node3D
+
+var ROCK : PackedScene = preload('res://scenes/asteroid_scenes/asteroids.tscn')
+var LONG_ROCK : PackedScene = preload('res://scenes/asteroid_scenes/long_asteroid.tscn')
+var SUN : PackedScene = preload('res://scenes/star.tscn')
+var rng = RandomNumberGenerator.new()
+var SUN_added = false
+var x = 90
+var played = false;
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#for i in range(0, 90):
+	#	rockSpawn(Vector3(rng.randf_range(-100.0, 100.0), rng.randf_range(-100.0, 100.0), rng.randf_range(-100.0, 100.0)))
+	played = true;
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	
+	if (!SUN_added):
+		var instance = SUN.instantiate()
+		instance.position = Vector3(100.0, 100.0, 0.0)
+		instance.scale *= 1
+		get_parent().add_child(instance)
+		SUN_added = true
+	if (x > 0) :
+		x -= 1
+		
+		rockSpawn(Vector3(rng.randf_range(-500.0, 500.0), rng.randf_range(-500.0, 500.0), rng.randf_range(-500.0, 500.0)))
+
+func rockSpawn(pos: Vector3):
+	var instance = ROCK.instantiate()
+	instance.position = pos
+	instance.scale *= 8
+	get_parent().add_child(instance)
