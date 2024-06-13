10707dbe2d50d67d953820d514336ddda0311058
matousposp08 <154290575+matousposp08@users.noreply.github.com>
2024-06-12 21:40:55 -0400

    finish options menu

---
+++ Dogfights/options.gd
@@ -0,0 +1,20 @@
+extends Node2D
+var master_bus = AudioServer.get_bus_index("Master")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$HSlider.value = 0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(master_bus, value)
+	if value == -30:
+		AudioServer.set_bus_mute(master_bus, true)
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
--- Dogfights/scenes/plane_ui.gd
+++ Dogfights/scenes/plane_ui.gd
@@ -17,3 +17,7 @@ func _on_button_pressed():
 
 func _on_button_2_pressed():
 	get_tree().change_scene_to_file("res://scenes/multiplayerlvl_1.tscn")
+
+
+func _on_button_3_pressed():
+	get_tree().change_scene_to_file("res://options.tscn")
