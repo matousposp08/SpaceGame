3d7251ed485d2f0aa3e42a8d3a527f15affce1a0
Nikhil Palempalle <nikpal26@bergen.org>
2024-06-13 07:50:19 -0400

    level5

--- Dogfights/scenes/planet.gd
+++ Dogfights/scenes/planet.gd
@@ -4,13 +4,13 @@ extends RigidBody3D
 var unclaimed: Color = Color(0.41, 0.41, 0.41, 1.0)
 var blue: Color = Color(0.12, 0.64, 0.70, 1.0)
 var red: Color = Color(0.75, 0.11, 0.11, 1.0)
-var rotation_speed: float = 1.0
 #var planet_color = Vector3(105.0, 105.0, 105.0) #Unclaimed color
 
 # Called when the node enters the scene tree for the first time.
 func switch():
 	if (player_num == 0):
 		set_color(blue)
+		#sun_pos = 
 	else:
 		set_color(red)
 		
@@ -19,9 +19,7 @@ func _ready():
 	pass # Replace with function body.
 
 func set_color(color):
-	var newMaterial = $MeshInstance3D.StandardMaterial3D.new()
-	newMaterial.albedo_color = color
-	$MeshInstance3D.material_override = newMaterial
+	$MeshInstance3D.set_instance_shader_parameter("albedo", Vector4(color.r, color.g, color.b, 1.0))
 	pass
 	
 func _physics_process(delta):
