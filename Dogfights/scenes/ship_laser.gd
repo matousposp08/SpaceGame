8429f6e26938c921b6b9d008ce770090136649b8
finsld <142837936+dVillarosa287@users.noreply.github.com>
2024-06-13 09:57:04 -0400

    menu: add multiplayer lvl select

--- Dogfights/scenes/ship_laser.gd
+++ Dogfights/scenes/ship_laser.gd
@@ -42,7 +42,7 @@ func is_colliding() -> bool:
 
 
 func _on_area_3d_area_entered(area):
-	if not is_in_group("player"):
+	if get_parent().name == "multiplayer_lvl1" and not is_in_group("player"):
 		if area.is_in_group("player"):
 			print(get_parent().name)
 			get_parent().get_node("update").text = area.get_parent().name + " blast " + str(randi_range(0,1000))
--- Dogfights/scripts/multilvlselect.gd
+++ Dogfights/scripts/multilvlselect.gd
@@ -12,12 +12,11 @@ func _process(delta):
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/lvlselect.tscn")
+	get_tree().change_scene_to_file("res://scenes/multiplayerlvl_1.tscn")
 
 
 func _on_button_2_pressed():
-	get_tree().change_scene_to_file("res://scenes/multilvlselect.tscn")
+	get_tree().change_scene_to_file("res://scenes/multiplayerlvl_2.tscn")
 
-
-func _on_button_3_pressed():
-	get_tree().change_scene_to_file("res://options.tscn")
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://menu.tscn")
