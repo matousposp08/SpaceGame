aaadcd7339cb3db418521e272d8c9fbb0fc067a6
EvanC22 <evanchin0322@gmail.com>
2024-06-12 21:02:44 -0400

    win screen lvl 4

--- Dogfights/scripts/lvl_4.gd
+++ Dogfights/scripts/lvl_4.gd
@@ -1,13 +1,25 @@
 extends Node3D
 
-@onready var player = $ship
-@onready var tunnel = $Area3D/MeshInstance3D
-
+var SUCCESS : PackedScene = preload('res://scenes/success.tscn')
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
+@onready var ship = $ship
+
+func _ready():
+	#for i in range(0, 90):
+	#	rockSpawn(Vector3(rng.randf_range(-100.0, 100.0), rng.randf_range(-100.0, 100.0), rng.randf_range(-100.0, 100.0)))
+
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	
+	#if (!SUN_added):
+		#var instance = SUN.instantiate()
+		#instance.position = Vector3(100.0, 100.0, 0.0)
+		#instance.scale *= 1
+		#get_parent().add_child(instance)
+		#SUN_added = true
+	if (ship.position.z >= 460) :
+		var instance = SUCCESS.instantiate()
+		get_parent().add_child(instance)
