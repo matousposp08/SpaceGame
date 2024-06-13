85ad65bf17296aaf650fce95b890cae6e2549dd7
matousposp08 <154290575+matousposp08@users.noreply.github.com>
2024-06-13 09:11:24 -0400

    back buttons

--- Dogfights/scenes/lvlselect.gd
+++ Dogfights/scenes/lvlselect.gd
@@ -25,3 +25,7 @@ func _on_button_3_pressed():
 
 func _on_button_4_pressed():
 	get_tree().change_scene_to_file("res://scenes/lvl_4.tscn")
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://menu.tscn")
