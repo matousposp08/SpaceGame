6780ea4214cb71c542a3b4a7d67a260fa363389e
EvanC22 <evanchin0322@gmail.com>
2024-06-13 07:30:37 -0400

    back button for menu

--- Dogfights/options.gd
+++ Dogfights/options.gd
@@ -18,3 +18,7 @@ func _on_h_slider_value_changed(value):
 		AudioServer.set_bus_mute(master_bus, true)
 	else:
 		AudioServer.set_bus_mute(master_bus, false)
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://menu.tscn")
